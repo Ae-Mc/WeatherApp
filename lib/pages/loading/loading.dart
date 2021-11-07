c1b92a7259031dd58ce03ea967ebb0565205cd34
Ae-Mc <ae_mc@mail.ru>
2021-11-08 00:41:29 +0300

    Update loading page initialization logic.
    Fix reading context of disposed widget.
    Add initializeDateFormatting call from intl package.

--- lib/pages/loading/loading.dart
+++ lib/pages/loading/loading.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:provider/provider.dart';
 import 'package:weather_app/data/providers/weather_provider.dart';
 import 'package:weather_app/data/storage/storage.dart';
@@ -8,17 +9,22 @@ import 'package:weather_app/router/router.gr.dart';
 class LoadingPage extends StatelessWidget {
   const LoadingPage({Key? key}) : super(key: key);
 
+  Future<void> futures(BuildContext context) async {
+    var router = AutoRouter.of(context);
+    await Future.wait([
+      Storage.initialize().then((value) =>
+          Provider.of<WeatherProvider>(context, listen: false).initialize()),
+      initializeDateFormatting('ru_RU'),
+    ]);
+    router.replace(const HomeRoute());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: FutureBuilder(
-            future: Future.wait([
-              Storage.initialize().then((value) =>
-                  Provider.of<WeatherProvider>(context, listen: false)
-                      .initialize()),
-            ])
-              ..then((_) => AutoRouter.of(context).replace(const HomeRoute())),
+            future: futures(context),
             builder: (context, snapshot) {
               return SizedBox.expand(
                 child: Column(
