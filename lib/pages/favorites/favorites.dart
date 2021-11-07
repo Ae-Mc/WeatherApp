51ea9e49fd2fb3126794a777733d63d482b183b8
Ae-Mc <ae_mc@mail.ru>
2021-11-08 00:55:43 +0300

    Add onTap listener for favorites page items.
    Now it's possible to select active place from favorites.

--- lib/pages/favorites/favorites.dart
+++ lib/pages/favorites/favorites.dart
@@ -2,7 +2,10 @@ import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:provider/provider.dart';
 import 'package:weather_app/data/models/place.dart';
+import 'package:weather_app/data/providers/settings_provider.dart';
+import 'package:weather_app/data/providers/weather_provider.dart';
 import 'package:weather_app/data/storage/storage.dart';
 import 'package:weather_app/widgets/page_header.dart';
 
@@ -42,63 +45,72 @@ class _FavoritesPageState extends State<FavoritesPage> {
 
   Widget _favoriteRow(Place place) {
     return Builder(builder: (context) {
-      return Stack(
-        children: [
-          SizedBox(
-            width: double.infinity,
-            child: Neumorphic(
-              style: NeumorphicStyle(
-                color: Theme.of(context).colorScheme.surface,
-                depth:
-                    Theme.of(context).colorScheme.brightness == Brightness.light
-                        ? -4
-                        : 3,
-                lightSource: LightSource.top,
-                boxShape:
-                    NeumorphicBoxShape.roundRect(BorderRadius.circular(15)),
-              ),
-              child: Padding(
-                padding: const Pad(all: 16),
-                child: Text(
-                    place.country == null
-                        ? place.name
-                        : '${place.country}, ${place.name}',
-                    style: Theme.of(context).textTheme.overline),
+      return InkWell(
+        borderRadius: BorderRadius.circular(25),
+        onTap: () async {
+          context.read<SettingsProvider>().activePlace = place;
+          await context.read<WeatherProvider>().updateWeatherData();
+          Navigator.of(context).pop();
+        },
+        child: Stack(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: Neumorphic(
+                style: NeumorphicStyle(
+                  color: Theme.of(context).colorScheme.surface,
+                  depth: Theme.of(context).colorScheme.brightness ==
+                          Brightness.light
+                      ? -4
+                      : 3,
+                  lightSource: LightSource.top,
+                  boxShape:
+                      NeumorphicBoxShape.roundRect(BorderRadius.circular(15)),
+                ),
+                child: Padding(
+                  padding: const Pad(all: 16),
+                  child: Text(
+                      place.country == null
+                          ? place.name
+                          : '${place.country}, ${place.name}',
+                      style: Theme.of(context).textTheme.overline),
+                ),
               ),
             ),
-          ),
-          Positioned(
-            top: 0,
-            bottom: 0,
-            right: 0,
-            child: AspectRatio(
-              aspectRatio: 1,
-              child: GestureDetector(
-                onTap: () {
-                  Storage.removeFavorite(place).then((_) => setState(() => {}));
-                },
-                child: Neumorphic(
-                  style: NeumorphicStyle(
-                    color: Theme.of(context).colorScheme.brightness ==
-                            Brightness.light
-                        ? const Color(0xFFC8DAFF)
-                        : const Color(0xFF152A53),
-                    depth: Theme.of(context).colorScheme.brightness ==
-                            Brightness.light
-                        ? 4
-                        : 0,
-                    boxShape:
-                        NeumorphicBoxShape.roundRect(BorderRadius.circular(15)),
-                  ),
-                  child: Icon(
-                    Icons.close,
-                    color: Theme.of(context).colorScheme.onBackground,
+            Positioned(
+              top: 0,
+              bottom: 0,
+              right: 0,
+              child: AspectRatio(
+                aspectRatio: 1,
+                child: GestureDetector(
+                  onTap: () {
+                    Storage.removeFavorite(place)
+                        .then((_) => setState(() => {}));
+                  },
+                  child: Neumorphic(
+                    style: NeumorphicStyle(
+                      color: Theme.of(context).colorScheme.brightness ==
+                              Brightness.light
+                          ? const Color(0xFFC8DAFF)
+                          : const Color(0xFF152A53),
+                      depth: Theme.of(context).colorScheme.brightness ==
+                              Brightness.light
+                          ? 4
+                          : 0,
+                      boxShape: NeumorphicBoxShape.roundRect(
+                          BorderRadius.circular(15)),
+                    ),
+                    child: Icon(
+                      Icons.close,
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       );
     });
   }
