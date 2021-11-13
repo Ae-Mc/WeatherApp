d0e3c0ecd8c4f02c262151338a2b6d4213f4413f
Ae-Mc <ae_mc@mail.ru>
2021-11-13 21:06:40 +0300

    Create Settings entity

---
+++ lib/features/settings/domain/entities/settings.dart
@@ -0,0 +1,50 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:weather_app/features/search/domain/entities/place.dart';
+
+class Settings extends Equatable {
+  final TemperatureUnits temperatureUnits;
+  final SpeedUnits speedUnits;
+  final PressureUnits pressureUnits;
+  final ThemeMode themeMode;
+  final Place activePlace;
+  final List<Place> favorites;
+
+  const Settings({
+    required this.temperatureUnits,
+    required this.speedUnits,
+    required this.pressureUnits,
+    required this.themeMode,
+    required this.activePlace,
+    required this.favorites,
+  });
+
+  const Settings.defaultSettings()
+      : temperatureUnits = TemperatureUnits.celcius,
+        speedUnits = SpeedUnits.metersPerSecond,
+        pressureUnits = PressureUnits.mmOfMercury,
+        themeMode = ThemeMode.system,
+        activePlace = const Place(
+          name: 'Санкт-Петербург',
+          latitude: 59.93863,
+          longitude: 30.31413,
+          population: -1,
+        ),
+        favorites = const [];
+
+  @override
+  List<Object?> get props => [
+        temperatureUnits,
+        speedUnits,
+        pressureUnits,
+        themeMode,
+        activePlace,
+      ];
+
+  @override
+  bool? get stringify => true;
+}
+
+enum TemperatureUnits { celcius, farenheit }
+enum SpeedUnits { metersPerSecond, kilometersPerHour }
+enum PressureUnits { mmOfMercury, hectopascal }
