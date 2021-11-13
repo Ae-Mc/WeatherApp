c15f59b7c8c8f16e164aa2150822b130246e2321
Ae-Mc <ae_mc@mail.ru>
2021-11-14 00:37:54 +0300

    Create SettingsEvents

---
+++ lib/features/settings/presentation/bloc/settings_event.dart
@@ -0,0 +1,47 @@
+part of 'settings_bloc.dart';
+
+abstract class SettingsEvent extends Equatable {
+  const SettingsEvent();
+}
+
+class AppStarted extends SettingsEvent {
+  @override
+  List<Object?> get props => [];
+}
+
+abstract class SinglePropertyEvent<T> extends SettingsEvent {
+  final T property;
+
+  const SinglePropertyEvent(this.property);
+
+  @override
+  List<Object?> get props => [property];
+}
+
+class SetTemperatureUnits extends SinglePropertyEvent<TemperatureUnits> {
+  const SetTemperatureUnits(TemperatureUnits units) : super(units);
+}
+
+class SetSpeedUnits extends SinglePropertyEvent<SpeedUnits> {
+  const SetSpeedUnits(SpeedUnits units) : super(units);
+}
+
+class SetPressureUnits extends SinglePropertyEvent<PressureUnits> {
+  const SetPressureUnits(PressureUnits units) : super(units);
+}
+
+class SetThemeMode extends SinglePropertyEvent<ThemeMode> {
+  const SetThemeMode(ThemeMode themeMode) : super(themeMode);
+}
+
+class SetActivePlace extends SinglePropertyEvent<Place> {
+  const SetActivePlace(Place place) : super(place);
+}
+
+class AddFavorite extends SinglePropertyEvent<Place> {
+  const AddFavorite(Place place) : super(place);
+}
+
+class RemoveFavorite extends SinglePropertyEvent<Place> {
+  const RemoveFavorite(Place place) : super(place);
+}
