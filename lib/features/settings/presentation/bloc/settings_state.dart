e625955a423f1809db4385d83d57de8bd5a1aaf0
Ae-Mc <ae_mc@mail.ru>
2021-11-14 00:37:33 +0300

    Create SettingsStates

---
+++ lib/features/settings/presentation/bloc/settings_state.dart
@@ -0,0 +1,33 @@
+part of 'settings_bloc.dart';
+
+abstract class SettingsState extends Equatable {
+  const SettingsState();
+}
+
+class Empty extends SettingsState {
+  @override
+  List<Object> get props => [];
+}
+
+class Loading extends SettingsState {
+  @override
+  List<Object?> get props => [];
+}
+
+class Loaded extends SettingsState {
+  final Settings settings;
+
+  const Loaded(this.settings);
+
+  @override
+  List<Object?> get props => [settings];
+}
+
+class Error extends SettingsState {
+  final String message;
+
+  const Error(this.message);
+
+  @override
+  List<Object?> get props => [];
+}
