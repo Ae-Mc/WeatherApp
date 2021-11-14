f4f1c39fe455766cb5fd6d1b7f81cf43454ebd34
Ae-Mc <ae_mc@mail.ru>
2021-11-14 22:04:50 +0300

    Add SettingsBloc provider and use it in MaterialApp
    Add its initialization to loading page

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
 import 'package:weather_app/data/providers/settings_provider.dart';
 import 'package:weather_app/data/providers/weather_provider.dart';
+import 'package:weather_app/features/settings/presentation/bloc/settings_bloc.dart';
 import 'package:weather_app/router/router.gr.dart';
 import 'package:weather_app/theme/style.dart';
+
+final getIt = GetIt.instance;
 
 class App extends StatelessWidget {
   App({Key? key}) : super(key: key);
@@ -13,32 +18,44 @@ class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => SettingsProvider()),
-        ChangeNotifierProxyProvider<SettingsProvider, WeatherProvider>(
-          create: (_) => WeatherProvider(),
-          update: (_, settings, weather) {
-            if (weather == null) {
-              throw ArgumentError.notNull('weather');
-            }
-            weather.settings = settings;
-            return weather;
-          },
-        ),
-      ],
-      builder: (context, _) {
-        final style = Style();
-        return MaterialApp.router(
-          title: 'Weather app',
-          theme: style.lightTheme,
-          darkTheme: style.darkTheme,
-          themeMode: context
-              .select<SettingsProvider, ThemeMode>((value) => value.themeMode),
-          routerDelegate: _appRouter.delegate(),
-          routeInformationParser: _appRouter.defaultRouteParser(),
-        );
-      },
+    return BlocProvider(
+      create: (_) => SettingsBloc(),
+      lazy: true,
+      child: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => SettingsProvider()),
+          ChangeNotifierProxyProvider<SettingsProvider, WeatherProvider>(
+            create: (_) => WeatherProvider(),
+            update: (_, settings, weather) {
+              if (weather == null) {
+                throw ArgumentError.notNull('weather');
+              }
+              weather.settings = settings;
+              return weather;
+            },
+          ),
+        ],
+        builder: (context, _) {
+          final style = Style();
+          return BlocBuilder<SettingsBloc, SettingsState>(
+            buildWhen: (oldState, newState) {
+              return newState is Loaded;
+            },
+            builder: (context, state) {
+              return MaterialApp.router(
+                title: 'Weather app',
+                theme: style.lightTheme,
+                darkTheme: style.darkTheme,
+                themeMode: (state is Loaded)
+                    ? state.settings.themeMode
+                    : ThemeMode.dark,
+                routerDelegate: _appRouter.delegate(),
+                routeInformationParser: _appRouter.defaultRouteParser(),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
--- lib/pages/loading/loading.dart
+++ lib/pages/loading/loading.dart
@@ -1,16 +1,31 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:weather_app/data/providers/providers.dart';
 import 'package:weather_app/data/providers/weather_provider.dart';
 import 'package:weather_app/data/storage/storage.dart';
+import 'package:weather_app/features/settings/data/datasources/settings_local_data_source_impl.dart';
+import 'package:weather_app/features/settings/data/repositories/settings_repository_impl.dart';
+import 'package:weather_app/features/settings/presentation/bloc/settings_bloc.dart';
 import 'package:weather_app/router/router.gr.dart';
 
 class LoadingPage extends StatelessWidget {
   const LoadingPage({Key? key}) : super(key: key);
 
-  Future<void> futures(BuildContext context) async {
+  Future<void> init(BuildContext context) async {
+    BlocProvider.of<SettingsBloc>(context).add(
+      Init(
+        SettingsRepositoryImpl(
+          localDataSource: SettingsLocalDataSourceImpl(
+            await SharedPreferences.getInstance(),
+          ),
+        ),
+      ),
+    );
+
     var router = AutoRouter.of(context);
     var weatherProvider = context.read<WeatherProvider>();
     var settingsProvider = context.read<SettingsProvider>();
@@ -27,25 +42,24 @@ class LoadingPage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: FutureBuilder(
-            future: futures(context)
-              ..onError((error, stackTrace) => throw error!),
-            builder: (context, snapshot) {
-              return SizedBox.expand(
-                child: Column(
-                  children: [
-                    const Spacer(),
-                    Text('Weather',
-                        style: Theme.of(context).textTheme.headline2),
-                    const Spacer(),
-                    CircularProgressIndicator(
-                      color: Theme.of(context).colorScheme.onBackground,
-                    ),
-                    const Spacer(),
-                    const Spacer(),
-                  ],
-                ),
-              );
-            }),
+          future: init(context)..onError((error, stackTrace) => throw error!),
+          builder: (context, snapshot) {
+            return SizedBox.expand(
+              child: Column(
+                children: [
+                  const Spacer(),
+                  Text('Weather', style: Theme.of(context).textTheme.headline2),
+                  const Spacer(),
+                  CircularProgressIndicator(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+                  const Spacer(),
+                  const Spacer(),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
